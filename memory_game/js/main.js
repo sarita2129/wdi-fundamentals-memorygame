30cff568a74e4bdaa0eae5298d87ae6c0bdf7f6b
Sarita Nair <saritanair@192-168-1-103.tpgi.com.au>
2018-10-26 21:32:49 +1100

    created card game.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,6 +22,8 @@ suit : "diamonds",
 cardImage : "images/king-of-diamonds.png"
 }
 ];
+
+var score = 0;
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -29,25 +31,59 @@ var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
 		alert("You found a match!");
+		UpdateScore();
 	}else{
 		alert("Sorry, try again.");
 	}
 
 }
 
-var flipCard = function(cardId){
-var cardflip = cards[cardId].rank;
-cardsInPlay.push(cardflip);
+var flipCard = function(){
+	
+	if(cardsInPlay.length > 1)
+	{
+		resetCardsInPlay();
+	}
+var cardflip = this.getAttribute('data-id');
+cardsInPlay.push(cards[cardflip].rank);
+this.setAttribute('src',cards[cardflip].cardImage);
 
 if(cardsInPlay.length === 2)
 {
 checkForMatch();
 }
-console.log('user flipped ' + cardflip);
-console.log('image path ' + cards[cardId].cardImage);
-console.log('suit ' + cards[cardId].suit);
+console.log('user flipped ' + cards[cardflip].rank);
+console.log('image path ' + cards[cardflip].cardImage);
+console.log('suit ' + cards[cardflip].suit);
 }
 
+var resetCardsInPlay = function(){
+	var imgel = document.getElementsByTagName('img');
+	for(var cnt = 0;cnt < imgel.length; cnt++)
+	{
+		imgel[cnt].setAttribute('src','images/back.png');
+	}
+	cardsInPlay = [];
+}
+var resetGame = function(){
+	resetCardsInPlay();
+	score = 0;
+	document.getElementById('score').innerHTML = "";
+}
+var UpdateScore = function(){
+	score += 10;
+	document.getElementById('score').innerHTML = "Your total score is : <en>" + score + "</en>";
+}
+var createBoard = function(){
+	for(var cnt = 0; cnt < cards.length; cnt++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',cnt);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	document.getElementsByTagName('button')[0].addEventListener('click',resetGame);
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
